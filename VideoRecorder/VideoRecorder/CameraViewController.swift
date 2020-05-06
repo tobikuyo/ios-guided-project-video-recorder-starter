453d72ab86c8b49949c1f6dc1572c42e6061d7f1
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-06 18:25:47 +0100

    Added Live Camera Preview

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,20 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    lazy private var captureSession = AVCaptureSession()
+
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
-
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        setUpCaptureSession()
 	}
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession.startRunning()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+
+    // Live Preview
+
+    private func setUpCaptureSession() {
+        // Add inputs
+        captureSession.beginConfiguration()
+        // Camera input
+        let camera = bestCamera()
+
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera),
+            captureSession.canAddInput(cameraInput) else {
+                fatalError("Error adding camera to capture session")
+        }
+
+        captureSession.addInput(cameraInput)
+
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+
+
+
+        // Microphone input
+
+
+        // Add outputs
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+
+        // TODO: Start/Stop session
+    }
+
+    private func bestCamera() -> AVCaptureDevice {
+        // Ultra-wide lens (iPhone 11 Pro + Pro Max)
+        if let ultraWideCamera = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return ultraWideCamera
+        }
+        // Wide angle lens (available on any iPhone - front/ back)
+        if let wideAngleCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return wideAngleCamera
+        }
+
+        // No cameras present (simulator)
+        fatalError("No camera available - are you on a simulator?")
+    }
+
+    // Recording
 
     @IBAction func recordButtonPressed(_ sender: Any) {
 
